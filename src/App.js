ea139d46b14a050f59ab5b55a18f372d5cfdbb2f
Mitul Patel <mitul.srashtasoft@gmail.com>
2021-02-06 01:23:09 +0530

    g

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <img src={logo} className='App-logo' alt='logo' />
         <h1>HEY FOLKS</h1>
         <p>
           Edi t <code>src/App.js</code> and save to Deployed ...
